5e1b5d480c8707af315d58a145c7233dba9f516c
Maks <makso544g@gmail.com>
2025-06-19 19:50:59 +0100

    Added margin

--- frontend/maksymilian.dev/src/components/SectionInfoCard/SectionInfoCard.tsx
+++ frontend/maksymilian.dev/src/components/SectionInfoCard/SectionInfoCard.tsx
@@ -5,7 +5,7 @@ interface SectionInfoCardProps {
 
 const SectionInfoCard = ({ name, info }: SectionInfoCardProps) => {
   return (
-    <div className="section-info-card flex flex-col justify-center items-center ">
+    <div className="section-info-card flex flex-col justify-center items-center my-10">
       <h2 className="section-name text-xl font-bold text-gray-800">{name}</h2>
       <p className="section-info text-gray-400 ">{info}</p>
     </div>
