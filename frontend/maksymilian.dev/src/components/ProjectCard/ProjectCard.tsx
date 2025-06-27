fbadf4886a137a149d35f000c5c5a490ff104bd5
Maks <makso544g@gmail.com>
2025-06-27 19:58:16 +0100

    added href prop

--- frontend/maksymilian.dev/src/components/ProjectCard/ProjectCard.tsx
+++ frontend/maksymilian.dev/src/components/ProjectCard/ProjectCard.tsx
@@ -1,18 +1,26 @@
+import { FaLongArrowAltRight } from 'react-icons/fa';
+
 interface ProjectCardProps {
   imgPath: string;
   name: string;
+  href: string;
 }
 
-const ProjectCard = ({ imgPath, name }: ProjectCardProps) => {
+const ProjectCard = ({ imgPath, name, href }: ProjectCardProps) => {
   return (
     <div className="border border-gray-300 rounded-2xl bg-white drop-shadow-md p-5">
       <img
-        className="w-60 rounded-2xl py-2"
+        className="w-60 h-40 rounded-2xl py-2"
         src={`${imgPath}`}
         alt="Project Image"
       />
-      <h3 className="project-name text-lg">{name}</h3>
-      <p className="project-btn text-gray-700">View project</p>
+      <h3 className="project-name text-base 2xl:text-lg">{name}</h3>
+      <a href={href} className="project-btn container flex items-center gap-1 ">
+        <FaLongArrowAltRight className="text-gray-600" />
+        <p className="project-btn text-sm 2xl:text-base text-gray-700">
+          View project
+        </p>
+      </a>
     </div>
   );
 };
